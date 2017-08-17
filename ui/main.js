a70ffff7a3f3a9d1c62d2bf79a30a03fa693c9ba
YSS <yalavarthisainadh@gmail.com>
2017-08-17 19:56:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
-var value=document.getElementById("counter");
+var button=document.getElementById("counter");
 var counter=0;
-value.onclick=function(){
+button.onclick=function(){
     counter=counter+1;
     var span=document.getElementById("count");
     span.innerHTML=count.toString();
